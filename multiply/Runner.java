9c11bf87dad22ecc0bda883ee1d37e7e300ffce3
MikitaSychou <159012466+MikitaSychou@users.noreply.github.com>
2024-03-12 09:02:18 +0300

    Update Runner.java
    Comment is added

--- multiply/Runner.java
+++ multiply/Runner.java
@@ -2,6 +2,15 @@ package multiply;
 
 public class Runner {
     public static void main(String[] args) {
+        
+         // Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised
+        //to the number of digits of each numbers, so, for example:
+        //   3 -->    15  (  3 * 5¹)
+        //  10 -->   250  ( 10 * 5²)
+        // 200 --> 25000  (200 * 5³)
+        //   0 -->     0  (  0 * 5¹)
+        //  -3 -->   -15  ( -3 * 5¹)
+        
         System.out.println(multiply(3));
     }
 
