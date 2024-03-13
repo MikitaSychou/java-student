28bd90c892b2ee50d8dfd96fb1742b4ec788ae4d
Mikita Sychou <mikita.sychou@gmail.com>
2024-03-13 16:49:24 +0300

    updates in lost directory

--- lost/LostWithoutMap.java
+++ lost/LostWithoutMap.java
@@ -9,9 +9,9 @@ public class LostWithoutMap {
     }
 
         public static int[] map(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] += arr[i];
-        }
-        return arr;
+        return Arrays.stream(arr)
+                .map(el -> el * 2)
+                .toArray();
+
     }
 }
